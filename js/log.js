c3df8113a9835e792fae35dc11547f034ff0e2a6
Michael Rupp <michael.rupp@agis.ch>
2025-01-25 20:25:56 +0100

    window.iffdebug

--- js/log.js
+++ js/log.js
@@ -1,9 +1,9 @@
 // only DOM interaction is here and only for debug purpose
 
-const debug = true
+window.iffdebug = false
 
 export function	log(out) {
-	if (debug) console.log('IFF:', out)
+	if (window.iffdebug) console.log('IFF:', out)
 	const dbg_DOM = document.getElementById('info')
 	if (dbg_DOM) dbg_DOM.innerHTML += out +'<br/>'
 }
